5d8c74a1484d3823c0af968fe81d79212ca0a330
Dan K <itsdanya@gmail.com>
2015-12-10 23:07:57 +0300

    Add user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,7 +15,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:foo)
+  end
+
+  test 'email is downcased before validations' do
+    @user.email = 'FOO@BAR.COM'
+    @user.valid?
+    assert_equal 'foo@bar.com', @user.email
+  end
+
+  test 'email uniqueness' do
+    dup = @user.dup
+    assert @user.invalid?
+  end
+
+  test 'phone number is normalized' do
+    @user.phone_number = '7 123 456 78 90'
+    @user.valid?
+    assert_equal '+71234567890', @user.phone_number
+  end
+
+  test 'phone number uniqueness' do
+    user = users(:baz)
+    user.phone_number = @user.phone_number + ' '
+    assert user.invalid?
+  end
 end
