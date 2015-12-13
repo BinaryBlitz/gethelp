6f598de5315eeabfe30d564b6723d2cc95f66a55
Dan K <itsdanya@gmail.com>
2015-12-13 23:44:56 +0300

    Fix helper generator configuration

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module GetHelp
     config.active_record.raise_in_transactional_callbacks = true
 
     config.generators do |g|
-      g.helpers = false
+      g.helper = false
       g.assets = false
     end
   end
