4c1589f879da21477643ff4f71b8e3af93515a29
Dan K <itsdanya@gmail.com>
2015-12-14 00:16:27 +0300

    Remove foreign key on users from orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213202930) do
+ActiveRecord::Schema.define(version: 20151213211518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,5 +48,4 @@ ActiveRecord::Schema.define(version: 20151213202930) do
     t.datetime "updated_at",   null: false
   end
 
-  add_foreign_key "orders", "users"
 end
