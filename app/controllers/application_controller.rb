8aef68ccf9e1d25fbd18252b6bfe58e064496264
Dan K <itsdanya@gmail.com>
2015-12-14 00:13:29 +0300

    Restrict access by API token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  before_action :restrict_access
+  helper_method :current_user
+
+  protected
+
+  attr_reader :current_user
+
+  def restrict_access
+    unless restrict_access_by_params
+      render json: { message: 'Invalid API Token' }, status: 401
+      return
+    end
+  end
+
+  def restrict_access_by_params
+    return true if @current_user
+
+    @current_user = User.find_by_api_token(params[:api_token])
+  end
 end
