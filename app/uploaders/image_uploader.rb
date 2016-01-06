fdd17b9d7a773f760f327aeb6352749db4705747
Dan K <itsdanya@gmail.com>
2016-01-06 23:16:17 +0300

    Message image thumbnails

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,6 +11,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  version :thumb do
+    process resize_to_limit: [300, 300]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
--- app/views/messages/index.jbuilder
+++ app/views/messages/index.jbuilder
@@ -1,3 +1,4 @@
 json.array! @messages do |message|
   json.extract! message, :id, :content, :category, :order_id, :image_url, :created_at
+  json.image_thumb_url message.image.thumb.url
 end
