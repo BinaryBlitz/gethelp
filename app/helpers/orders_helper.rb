611a124234472ee7e68664f608ee216d3217987e
Dan K <itsdanya@gmail.com>
2016-01-19 02:46:57 +0300

    Admin: add colors to status labels

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,11 @@
 module OrdersHelper
+  def class_for_status(status)
+    case status
+    when 'new' then 'label-primary'
+    when 'pending' then 'label-warning'
+    when 'paid' then 'label-success'
+    when 'rejected', 'refunded' then 'label-danger'
+    else 'label-default'
+    end
+  end
 end
