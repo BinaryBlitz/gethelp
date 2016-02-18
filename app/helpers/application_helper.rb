632deabd245dd2c5d7d897a6413a74cf080705b1
Dan K <itsdanya@gmail.com>
2016-02-19 00:15:43 +0300

    Landing page fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def page_needs_refresh?
-    admin_signed_in? && current_admin.admin? && controller_name == 'orders'
+    admin_signed_in? && current_admin.admin? && controller_name == 'orders' && action_name == 'index'
   end
 end
