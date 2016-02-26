f7474ea42638c2e62428b36b75f3fbf63edb7632
Dan K <itsdanya@gmail.com>
2016-02-26 16:59:27 +0300

    Revert "Set default time zone to Moscow"

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module GettHelp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Moscow'
+    # config.time_zone = 'Central Time (US & Canada)'
 
     config.assets.precompile += %w(landing.css)
 
