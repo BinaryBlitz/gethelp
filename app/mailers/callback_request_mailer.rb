f3375cb3005e7ea56fd3f17306454197f3461f0e
SumLare <mrhard95@gmail.com>
2017-04-27 01:12:58 +0300

    Add subject to callback request mailer

--- app/mailers/callback_request_mailer.rb
+++ app/mailers/callback_request_mailer.rb
@@ -1,6 +1,9 @@
 class CallbackRequestMailer < ApplicationMailer
   def new_callback_request(callback_request)
     @callback_request = callback_request
-    mail(to: Rails.application.secrets.recipient_mail)
+    mail(
+      to: Rails.application.secrets.recipient_mail,
+      subject: "Заказ на обратный звонок №#{@callback_request.id}"
+    )
   end
 end
