cda8eb6069c8b1bbba2f868dd79ab2cdf0d7cd45
SumLare <mrhard95@gmail.com>
2017-04-27 00:08:45 +0300

    Add mailers previews

--- test/mailers/previews/callback_request_mailer_preview.rb
+++ test/mailers/previews/callback_request_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/callback_request_mailer
 class CallbackRequestMailerPreview < ActionMailer::Preview
-
+  def new_callback_request
+    CallbackRequestMailer.new_callback_request(CallbackRequest.first)
+  end
 end
