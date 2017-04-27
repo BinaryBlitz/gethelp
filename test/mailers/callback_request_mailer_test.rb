c449e0983cbabe1eb3f5a8cf898d7d423f1f1d8d
SumLare <mrhard95@gmail.com>
2017-04-27 23:39:53 +0300

    Order emails (#14)
    * Add recipient mail to secrets
    
    * Add mailgun domain to secrets
    
    * Crate callback request mailer
    
    * Create order mailer
    
    * Add mailers previews
    
    * Add subject to callback request mailer
    
    * Configure mailgun
    
    * Rename default mailer
    
    * Add recipient mail to secrets in dev and test environments
    
    * Add mailer tests

---
+++ test/mailers/callback_request_mailer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CallbackRequestMailerTest < ActionMailer::TestCase
+  setup do
+    @callback_request = callback_requests(:callback_request)
+  end
+
+  test 'new callback_request' do
+    email = CallbackRequestMailer.new_callback_request(@callback_request)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['orders@getthelp.ru'], email.from
+    assert_equal ['foo@bar.com'], email.to
+    assert_equal "Заказ на обратный звонок №#{@callback_request.id}", email.subject
+  end
+end
---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class OrderMailerTest < ActionMailer::TestCase
+  setup do
+    @order = orders(:order)
+  end
+
+  test 'new order' do
+    email = OrderMailer.new_order(@order)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['orders@getthelp.ru'], email.from
+    assert_equal ['foo@bar.com'], email.to
+    assert_equal "Заказ №#{@order.id}", email.subject
+  end
+end
